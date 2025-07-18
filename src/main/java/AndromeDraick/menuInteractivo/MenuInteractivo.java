d2d56a9e7559a263cbb013e6257911e90be49b71
AndromeDraick <yamtovsol@gmail.com>
2025-07-18 12:43:42 -0600

    Add files via upload

--- src/main/java/AndromeDraick/menuInteractivo/MenuInteractivo.java
+++ src/main/java/AndromeDraick/menuInteractivo/MenuInteractivo.java
@@ -51,7 +51,7 @@ public final class MenuInteractivo extends JavaPlugin {
 
         // 4) Configuración de tienda
         saveResource("config_tienda.yml", false);
-        saveResource("configuración/config_item_venta.yml", false);
+        saveResource("configuracion/config_item_venta.yml", false);
         configTienda = new ConfigTiendaManager(this);
         baseDeDatos = new GestorBaseDeDatos(this);
 

b0fca11bda06fc1c92431acb64ef2c706dc52b9e
AndromeDraick <yamtovsol@gmail.com>
2025-07-18 09:25:16 -0600

    Add files via upload

--- src/main/java/AndromeDraick/menuInteractivo/menu/EventosMenu.java
+++ src/main/java/AndromeDraick/menuInteractivo/menu/EventosMenu.java
@@ -23,6 +23,8 @@ public class EventosMenu implements Listener {
         String titulo = event.getView().getTitle();
 
         // Manejar clics en los distintos menús personalizados
+        MenuPrincipal.manejarClick(event);
+        MenuTrabajos .manejarClick(event);
         MenuTienda.manejarClick(event);
         MenuConfirmacion.manejarClick(event);
         MenuVentaVisual.manejarClick(event);
